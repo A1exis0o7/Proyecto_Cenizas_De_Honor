c2ffc74a3a4b8c5eac60907e1c2c57cd4768b201
MiguelangelNA2 <mnorenaarango@gmail.com>
2026-04-30 13:09:23 -0500

    puntos

--- src/Components/StatusMiguel.jsx
+++ src/Components/StatusMiguel.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const StatusMiguel = () => {
+export const StatusMiguel = () => {
+
+      const [puntos, setPuntos] = useState('');
+      <>
+      <div className='puntos'>
+        <button>100</button>
+        <button>100</button>
+        <button>200</button>
+      </div>
+      </>
   return (
     <div>StatusMiguel</div>
   )
 }
-
-export default StatusMiguel
