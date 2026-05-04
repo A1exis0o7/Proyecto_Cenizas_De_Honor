4287cb45eeb193d6f0d21a8aa4bce2a90f63c150
MiguelangelNA2 <mnorenaarango@gmail.com>
2026-05-04 13:10:14 -0500

    status miguel import

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,20 @@
 import React from 'react'
-<<<<<<< HEAD
 import { StatusMiguel } from './Components/StatusMiguel'
 
 const App = () => {
   return (
     <>
 
-    <StatusMiguel></>
+    <StatusMiguel/>
 
     </>
 
-=======
 import StatusAlex from './Components/StatusAlex'
 
 const App = () => {
   return (
     <div>App</div>
     <StatusAlex/>
->>>>>>> 95e457d42058ff8ae4e543886c6951c9001c1001
   )
 }
 
