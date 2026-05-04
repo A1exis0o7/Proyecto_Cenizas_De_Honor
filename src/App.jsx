5b877c9b9822691f182af76f6a839b71de78d12d
IsaLol1A <123charaagudelo@gmail.com>
2026-05-04 13:17:42 -0500

    Agregar Import q se me olvido - Isabel

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { StatusMiguel } from './Components/StatusMiguel'
 import StatusAlex from './Components/StatusAlex'
+import { StatusPersonajes_Isabel } from './Components/StatusPersonajes_Isabel'
 const App = () => {
   return (
   <>
