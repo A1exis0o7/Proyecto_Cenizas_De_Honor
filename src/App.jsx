f345d3218a7b9ffa35cff036cd4c47b561c9b264
lu-nrl <lunanicolle31@gmail.com>
2026-05-04 13:20:57 -0500

    lu status

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { StatusMiguel } from './Components/StatusMiguel'
 import StatusAlex from './Components/StatusAlex'
 import { StatusPersonajes_Isabel } from './Components/StatusPersonajes_Isabel'
+import {StatusGemas_Lu} from './Components/StatusGemas_Lu'
 const App = () => {
   return (
   <>
@@ -9,6 +10,7 @@ const App = () => {
     <StatusMiguel/>
     <StatusAlex/> 
     <StatusPersonajes_Isabel/>
+    <StatusGemas_Lu/>
 
   </>
   )
