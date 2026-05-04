ff24b5b7b88e2cb45b86ab95d8310b9c67ff9ec6
MiguelangelNA2 <mnorenaarango@gmail.com>
2026-05-04 13:08:15 -0500

    import status miguel

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { StatusMiguel } from './Components/StatusMiguel'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+
+    <StatusMiguel></>
+
+    </>
+
   )
 }
 
