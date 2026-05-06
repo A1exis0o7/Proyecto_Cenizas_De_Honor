8828ceff53c207f97df2973a7f299efebd784781
IsaLol1A <123charaagudelo@gmail.com>
2026-05-06 13:17:34 -0500

    quitar llaves

--- src/Components/StatusPersonajes_Isabel.jsx
+++ src/Components/StatusPersonajes_Isabel.jsx
@@ -10,10 +10,10 @@ return (
 <h1>Personajes: {personajes}</h1>
 
 <div className="personajes">
-    <button onClick={() =>  {setPersonajes( (prev ) => { prev + 1} )}}> personaje 1 🐞 </button>
-    <button onClick={() =>  {setPersonajes( (prev ) => { prev + 1} )}}>personaje 2 *emoji*</button> 
-    <button onClick={() =>  {setPersonajes( (prev ) => { prev + 1} )}}>personaje 3 🫠 </button>
-    <button onClick={() =>  {setPersonajes( (prev ) => { prev + 1} )}}>personaje 4 </button>
+    <button onClick={() =>  {setPersonajes( prev  =>  prev + 1 )}}> personaje 1 🐞 </button>
+    <button onClick={() =>  {setPersonajes( prev  =>  prev + 1 )}}>personaje 2 *emoji*</button> 
+    <button onClick={() =>  {setPersonajes( prev  =>  prev + 1 )}}>personaje 3 🫠 </button>
+    <button onClick={() =>  {setPersonajes( prev  =>  prev + 1 )}}>personaje 4 </button>
 </div>
 
 </>
