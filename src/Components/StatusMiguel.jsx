e1991626d03d3ea7aca53f8e2829dc5ed4f9b082
MiguelangelNA2 <mnorenaarango@gmail.com>
2026-05-06 13:17:26 -0500

    funciona x2

--- src/Components/StatusMiguel.jsx
+++ src/Components/StatusMiguel.jsx
@@ -9,11 +9,11 @@ export const StatusMiguel = () => {
       <h1>Puntos: {puntos}</h1>
     <div className='puntos'>
         
-        <button onClick={ () => {setPuntos ((prev) => {prev + 100})}}>100</button>
-        <button onClick={ () => {setPuntos ((prev) => {prev + 100})}}>100 *emoji*</button>
-        <button onClick={ () => {setPuntos ((prev) => {prev + 200})}}>200 😒</button>
-        <button onClick={ () => {setPuntos ((prev) => {prev + 200})}}>200 😶‍🌫️🤍</button>
-        
+        <button onClick={ () => {setPuntos (prev => prev + 100)}}>100</button>
+        <button onClick={ () => {setPuntos (prev => prev + 100)}}>100 *emoji*</button>
+        <button onClick={ () => {setPuntos (prev => prev + 200)}}>200 😒</button>
+        <button onClick={ () => {setPuntos (prev => prev + 200)}}>200 😶‍🌫️🤍</button>
+
       </div>
       </>
   )
