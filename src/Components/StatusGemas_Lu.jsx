58ef48ad41cfa686b04165e9cb530f1ac97b12ad
lu-nrl <lunanicolle31@gmail.com>
2026-05-07 13:30:10 -0500

    card

--- src/Components/StatusGemas_Lu.jsx
+++ src/Components/StatusGemas_Lu.jsx
@@ -37,7 +37,16 @@ export const StatusGemas_Lu = () => {
       <h1>Personajes Rick and Morty (Luna) </h1>
 {characters.map((char, index) => (
   <li key={index}>{char.name}</li>
+  
 ))}
+
+<div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="..."> </img>
+  <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+  </div>
+</div>
+
     </>
   )
 }
