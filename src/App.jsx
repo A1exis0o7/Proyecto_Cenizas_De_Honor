2280d56f6cecff20dad5466e01d4ce07836dd42f
IsaLol1A <123charaagudelo@gmail.com>
2026-05-19 16:02:12 -0500

    tIeNdA - Isa

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { StatusPersonajes_Isabel } from './Components/StatusPersonajes_Isabel'
 import {StatusGemas_Lu} from './Components/StatusGemas_Lu' */
 import Contactenos from './Pages/Conocenos'
 import Marca from './Pages/Marca'
+import Tienda from './Pages/Tienda'
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
     <StatusPersonajes_Isabel/>
     <StatusGemas_Lu/> */}
     <Contactenos/>
+    <Tienda/>
 
   </>
   )
