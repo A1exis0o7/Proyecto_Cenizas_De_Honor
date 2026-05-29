b1b58ef5c839c4fe79cf523e9060f8f13e76de61
MiguelangelNA2 <mnorenaarango@gmail.com>
2026-05-28 19:14:24 -0500

    borrar react duplicado

--- src/Pages/Tienda.jsx
+++ src/Pages/Tienda.jsx
@@ -9,8 +9,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 } from 'react-icons/fa6'; */
 
 
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Tienda = () => {
 
