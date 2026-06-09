9e2d051bb4304a12fc98afdedb98301f6ba58f14
lu-nrl <lunanicolle31@gmail.com>
2026-06-09 16:58:42 -0500

    Bolita botón

--- src/Components/LottieLuna.jsx
+++ src/Components/LottieLuna.jsx
@@ -1,15 +1,35 @@
 import lottieReact from "lottie-react";
 import animacionLuna from "../assets/animacionLuna.json";
+import { useRef } from "react";
+
 
 const Lottie = lottieReact.default;
 
-const LottieLuna = () => {
-  return (
-    <Lottie
-      animationData={animacionLuna}
-      loop={true}
-    />
-  );
+export const LottieLuna = () => {
+
+    const lottieRef = useRef()
+    const reproducir = () => {
+        lottieRef.current.play()
+    }
+
+    return (
+        <>
+
+            <div onClick={reproducir}>
+                <Lottie
+                lottieRef={lottieRef}
+                    animationData={animacionLuna}
+                    loop={false}
+                    autoplay={false}
+                    style={{
+                        width: 500,
+                        heigth: 500,
+                    }}
+                />
+            </div>
+        </>
+
+    );
 };
 
 export default LottieLuna;
