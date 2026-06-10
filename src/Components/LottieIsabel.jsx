09693cb2f3889ee4bf228be4410032ae2f04fd9d
IsaLol1A <123charaagudelo@gmail.com>
2026-06-09 20:01:11 -0500

    Agregar useRef - Isa

--- src/Components/LottieIsabel.jsx
+++ src/Components/LottieIsabel.jsx
@@ -1,14 +1,34 @@
 import lottieReact from "lottie-react";
 import animacionIsabel from "../assets/animacionisabel.json";
+import { useRef } from "react";
 
 const Lottie = lottieReact.default;
 
 const LottieIsabel = () => {
+
+  const LottieRef = useRef();
+
+  const Reproducir = () => {
+    LottieRef.current.play()
+  }
+
   return (
-    <Lottie
-      animationData={animacionIsabel}
-      loop={true}
-    />
+
+    <>
+      <div onClick={Reproducir}>
+
+        <Lottie
+          LottieRef={LottieRef}
+          animationData={animacionIsabel}
+          loop={true}
+          autoPlay={false}
+          style={{ width: 500, height: 500 }}
+        />
+        
+      </div>
+
+    </>
+
   );
 };
 
