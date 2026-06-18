577c97f060361c2baea0b4e454619702ff0a8764
IsaLol1A <123charaagudelo@gmail.com>
2026-06-18 13:39:20 -0500

    Adelanto - Isa

--- src/Components/LottieIsabel.jsx
+++ src/Components/LottieIsabel.jsx
@@ -9,23 +9,26 @@ const Lottie = lottieReact.default;
 const LottieIsabel = () => {
   const LottieRef = useRef(null);
 
-  const Reproducir = () => {
+  const reproducir = () => {
     LottieRef.current?.goToAndPlay(0, true);
   }
 
   return (
 
     <div className="Pantalla">
+      
       <img className="position-absolute fondo" src="public/bbs/Fondo.png" alt="" />
       <img className="position-absolute nube1" src="public/bbs/Nube 1.png" alt=""/>
       <img className="position-absolute nube2" src="public/bbs/Nube 2.png" alt=""/>
       <img className="position-absolute cuboHielo" src="public/bbs/Cubo hielo.png" alt=""/>
       <img className="position-absolute pezEspinas" src="public/bbs/Pez espinas.png" alt=""/>
+      <img className="position-absolute iglu" src="public/bbs/Iglu.png" alt=""/>
+      
 
        <div className= "position-absolute osito" onClick = { reproducir} >
         <Lottie
           LottieRef={LottieRef}
-          animationData={animacionOsito-Isabel}
+          animationData={animacionIsabel}
           loop={false}
           autoPlay={false}
           style={{ width: 366, 
@@ -35,6 +38,18 @@ const LottieIsabel = () => {
         />
       </div>
 
+      <div className = "position-absolute pingui" onClick = { reproducir} >
+        <Lottie
+          LottieRef={LottieRef}
+          animationData={animacionPingui}
+          loop={false}
+          autoPlay={false}
+          style={{ width: 180, 
+                   height: 500,
+                   cursor: "pointer",}}
+
+        />
+      </div>
 
     </div>
 
