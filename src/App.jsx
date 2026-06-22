c9d7f22307f9727c4257d71ae9a06f254eb52350
A1exis0o7 <alexiselpro2019@gmail.com>
2026-06-22 15:41:11 -0500

    enlace a comic

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-/* import React from 'react'
-import { StatusMiguel } from './Components/StatusMiguel'
-import StatusAlex from './Components/StatusAlex'
-import { StatusPersonajes_Isabel } from './Components/StatusPersonajes_Isabel'
-import {StatusGemas_Lu} from './Components/StatusGemas_Lu' */
+
 
 import Conocenos from './Pages/Conocenos'
 import Home from './Pages/Home'
+import Comic from  './Pages/Comic'
 import Tienda from './Pages/Tienda'
 import Marca from './Pages/Marca'
 import LottieIsabel from './Components/LottieIsabel'
@@ -22,6 +19,7 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/Comic" element={<Comic />} />
           <Route path="/tienda" element={<Tienda />} />
           <Route path="/conocenos" element={<Conocenos />} />
           <Route path="/marca" element={<Marca />} />
