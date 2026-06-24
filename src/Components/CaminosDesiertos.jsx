e1dbcee0291eb8151b4f4892e1d2fd909a253d85
MiguelangelNA2 <mnorenaarango@gmail.com>
2026-06-24 14:33:34 -0500

    animacion funcionando

--- src/Components/CaminosDesiertos.jsx
+++ src/Components/CaminosDesiertos.jsx
@@ -1,23 +1,40 @@
-import React from 'react'
-import "../Components/StyleComic.css"
+import lottieReact from "lottie-react";
+import React, { useRef } from "react";
+import "../Components/StyleComic.css";
+import animacionLeon from "../assets/lotties miguel/sir leon-caminos-desiertos.json";
 
-
+const Lottie = lottieReact.default;
 
 const CaminosDesiertos = () => {
+
+  const leonRef = useRef(null);
+
+  const reproducir = () => {
+    leonRef.current?.goToAndPlay(0, true);
+  };
+
   return (
-    
-    <>
-    
+    <div className="Escenario-Completo Fondo">
 
-   <div className='Escenario-Completo Fondo'>
-    
-   </div>
+      <div
+        className="position-absolute oso"
+        onClick={reproducir}
+      >
+        <Lottie
+          lottieRef={leonRef}
+          animationData={animacionLeon}
+          loop={false}
+          autoplay={false}
+          style={{
+            width: 366,
+            height: 500,
+            cursor: "pointer",
+          }}
+        />
+      </div>
 
-    
-    </>
+    </div>
+  );
+};
 
-
-  )
-}
-
-export default CaminosDesiertos
+export default CaminosDesiertos;
