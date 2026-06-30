574caadd9ab67b5af11380458c15b15d3f5bf9d7
lu-nrl <lunaicolle31@gmail.com>
2026-06-30 16:06:03 -0500

    p

--- src/Components/LottieLlanuras.jsx
+++ src/Components/LottieLlanuras.jsx
@@ -1,7 +1,19 @@
 import React, { useRef } from "react";
 import animacionLeon from "../assets/AnimaciónLuna/Llanuraleón.json";
+import Lottie from "lottie-react";
 
 export const LottieLlanuras = () => {
+    const leonRef = useRef(null);
 
+    return (
+        <div className="contenedor-personajes">
+            <Lottie
+                lottieRef={leonRef}
+                animationData={animacionLeon}
+                loop={true}
+                autoplay={true}
+                
+            />
+        </div>);
     
 };
